406a0d1f22b5ae3126b9ab74882f52a8466fa4c4
tasuten <heliopause64@gmail.com>
2012-11-22 22:28:06 +0900

    Write formula for ttyload http://www.daveltd.com/src/util/ttyload/

---
+++ ttyload.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Ttyload < Formula
+  head  'git://github.com/lindes/ttyload.git'
+  homepage 'http://www.daveltd.com/src/util/ttyload/'
+  url 'git://github.com/lindes/ttyload.git'
+  version "0.5.2"
+
+  def install
+    system "make"
+    bin.install('ttyload')
+  end
+
+end
+
