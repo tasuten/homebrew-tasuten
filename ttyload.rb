af78580517d21cf69ab5d5a32c177498acfae442
tasuten <heliopause64@gmail.com>
2015-07-18 04:57:26 +0900

    ttyload 0.5.2 -> 0.5.3

--- ttyload.rb
+++ ttyload.rb
@@ -7,7 +7,7 @@ class Ttyload < Formula
   head  'git://github.com/lindes/ttyload.git'
   homepage 'http://www.daveltd.com/src/util/ttyload/'
   url 'git://github.com/lindes/ttyload.git'
-  version '0.5.2'
+  version '0.5.3'
 
   def install
     system "make"
