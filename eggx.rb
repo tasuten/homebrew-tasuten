44e90bc281c1de0889b16509261684c1839a30e9
tasuten <heliopause64@gmail.com>
2015-10-26 06:59:35 +0900

    Readd the formula for eggx
    Perhaps, I should not specify cmake

---
+++ eggx.rb
@@ -0,0 +1,25 @@
+class Eggx < Formula
+  homepage "http://www.ir.isas.jaxa.jp/~cyamauch/eggx_procall/"
+  url "http://www.ir.isas.jaxa.jp/~cyamauch/eggx_procall/eggx-0.93r5.tar.gz"
+  version "0.93r5"
+  sha256 "fd917f5f8b082f6812f9568cb8960481f88299a23afa372dc8a90851b785b731"
+
+  depends_on :x11
+
+  def install
+    system "make"
+    system "make install PREFIX=#{prefix}"
+  end
+
+  def caveats; <<-EOS.undent
+  You can get the EGGX's PDF manual from the website(brew home eggx).
+
+      English version:
+      http://www.ir.isas.jaxa.jp/~cyamauch/eggx_procall/eggx_procall.pdf
+      Japanese version:
+      http://www.ir.isas.jaxa.jp/~cyamauch/eggx_procall/eggx_procall.ja.pdf
+    EOS
+  end
+
+end
+
