87e7b48b7c53f0fd425a08b55372810f0adf654a
tasuten <heliopause64@gmail.com>
2015-10-26 06:08:11 +0900

    md5 is obsolated. Must use sha1 or sha256.

--- eggx.rb
+++ eggx.rb
@@ -6,7 +6,7 @@ require 'formula'
 class Eggx < Formula
   homepage 'http://www.ir.isas.jaxa.jp/~cyamauch/eggx_procall/'
   url 'http://www.ir.isas.jaxa.jp/~cyamauch/eggx_procall/eggx-0.93r5.tar.gz'
-  md5 '236f852a5d8d4591f730e439e5130cc0'
+  sha256 'fd917f5f8b082f6812f9568cb8960481f88299a23afa372dc8a90851b785b731'
   version '0.93r5'
 
   depends_on 'cmake' => :build
