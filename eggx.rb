da190f320eea4bf4500876c00b8a577bce8f6696
tasuten <heliopause64@gmail.com>
2015-10-26 07:04:44 +0900

    Use single-quote if possible

--- eggx.rb
+++ eggx.rb
@@ -1,8 +1,8 @@
 class Eggx < Formula
-  homepage "http://www.ir.isas.jaxa.jp/~cyamauch/eggx_procall/"
-  url "http://www.ir.isas.jaxa.jp/~cyamauch/eggx_procall/eggx-0.93r5.tar.gz"
-  version "0.93r5"
-  sha256 "fd917f5f8b082f6812f9568cb8960481f88299a23afa372dc8a90851b785b731"
+  homepage 'http://www.ir.isas.jaxa.jp/~cyamauch/eggx_procall/'
+  url 'http://www.ir.isas.jaxa.jp/~cyamauch/eggx_procall/eggx-0.93r5.tar.gz'
+  version '0.93r5'
+  sha256 'fd917f5f8b082f6812f9568cb8960481f88299a23afa372dc8a90851b785b731'
 
   depends_on :x11
 
