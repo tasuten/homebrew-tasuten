a53cc151c5358f47c7938f2590c4c56c40e82037
tasuten <heliopause64@gmail.com>
2015-11-08 23:09:53 +0900

    Add used-mem formula

---
+++ used-mem.rb
@@ -0,0 +1,13 @@
+class UsedMem < Formula
+  desc 'CLI Tool to print memory status'
+  head 'https://github.com/yonchu/used-mem.git'
+  homepage 'https://github.com/yonchu/used-mem'
+  url 'https://github.com/yonchu/used-mem.git'
+  version '1.0'
+
+  def install
+    bin.install('used-mem')
+  end
+
+end
+
