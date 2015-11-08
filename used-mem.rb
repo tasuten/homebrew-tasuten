f27cabc0c7004f9fa36ad4a16b76f06f56466557
tasuten <heliopause64@gmail.com>
2015-11-09 00:22:20 +0900

    Specify release revision

--- used-mem.rb
+++ used-mem.rb
@@ -2,7 +2,7 @@ class UsedMem < Formula
   desc 'CLI Tool to print memory status'
   head 'https://github.com/yonchu/used-mem.git'
   homepage 'https://github.com/yonchu/used-mem'
-  url 'https://github.com/yonchu/used-mem.git'
+  url 'https://github.com/yonchu/used-mem.git', :tag => '1.0'
   version '1.0'
 
   def install
